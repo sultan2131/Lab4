6f4a6ee5c32e030f4a2e25c35f0c5561a09c2e6e
Yevhenii Grytsay <60937148+sultan2131@users.noreply.github.com>
2022-11-10 12:57:15 +0200

    Add files via upload

---
+++ 4lab.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	const int n = 5;
+	int array[n] = {123, 879, 27, 89, 42};
+ 
+	for (int s = 0; s < n - 1; ++s)
+	{
+		int m = s;
+        int c
+		for (c = s + 1; c < n; ++c)
+		{
+			if (array[c] < array[m])
+				m = c;
+		}
+		swap(array[s], array[m]);
+	}
+ 
+	for (int i = 0; i < n; ++i)
+		cout << array[i] << ' ';
+ 
+	return 0;
+}
